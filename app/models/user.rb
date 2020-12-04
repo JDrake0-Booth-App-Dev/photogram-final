598553f84d365c667841535ffe60626143ca5573
jdrake0 <jdrake0@chicagobooth.edu>
2020-12-04 01:38:19 +0000

    Tables Set Up Correct

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  comments_count  :integer
+#  email           :string
+#  likes_count     :integer
+#  password_digest :string
+#  private         :boolean
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true
+  has_secure_password
+
+  has_many(:own_photos, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy })
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy })
+  has_many(:likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy })
+  has_many(:received_follow_requests, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
+  has_many(:sent_follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+  has_many(:following, { :through => :sent_follow_requests, :source => :recipient })
+  has_many(:followers, { :through => :received_follow_requests, :source => :sender })
+  validates(:username, { :uniqueness => true })
+end
