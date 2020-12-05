b569d5bf50bd1aa0fa1793ae5ae2fff4717bdddf
jdrake0 <jdrake0@chicagobooth.edu>
2020-12-05 18:44:06 +0000

    12.5.20 1300

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,13 +22,16 @@ class CommentsController < ApplicationController
     the_comment.author_id = params.fetch("query_author_id")
     the_comment.body = params.fetch("query_body")
     the_comment.photo_id = params.fetch("query_photo_id")
+    the_comment.save
 
-    if the_comment.valid?
-      the_comment.save
-      redirect_to("/comments", { :notice => "Comment created successfully." })
-    else
-      redirect_to("/comments", { :notice => "Comment failed to create successfully." })
-    end
+    redirect_to("/photos/#{the_comment.photo_id}")
+    #if the_comment.valid?
+    #  the_comment.save
+      #redirect_to("/comments", { :notice => "Comment created successfully." })
+      #redirect_to("/photos/#{the_comment.photo_id}", { :notice => "Comment created successfully." })
+    #else
+      #redirect_to("/photos/#{the_comment.photo_id}", { :notice => "Comment failed to create successfully." })
+    #end
   end
 
   def update
